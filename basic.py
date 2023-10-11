87feacb19f9f69b889eb9099aa39752f39ecb9f7
dmickelson <davidpmickelson@gmail.com>
2023-10-10 23:28:42 -0400

    updates

--- basic.py
+++ basic.py
@@ -1,7 +1,12 @@
 from autogen import AssistantAgent, UserProxyAgent, config_list_from_json
+from dotenv import load_dotenv
+import openai
+
+load_dotenv()
 
 # Import the openai api key
 config_list = config_list_from_json(env_or_file="OAI_CONFIG_LIST")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # Create assistant agent
 assistant = AssistantAgent(name="assistant", llm_config={
